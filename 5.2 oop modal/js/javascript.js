63137a3c234684014eaabe28e091f0deefdb7f0a
fhros <112174015+fhros@users.noreply.github.com>
2023-05-02 13:33:14 +0300

    Update javascript.js

--- 5.2 oop modal/js/javascript.js
+++ 5.2 oop modal/js/javascript.js
@@ -1,10 +1,13 @@
+//global variable
 var modal = document.getElementById("modal")
 
+//button for opening modal
 document.getElementById("btn").addEventListener("click", function(){
     document.getElementById("testing").style.display = "block"
     modal.style.display = "block"
 })
 
+//function to click backgound to close modal
 document.getElementById("testing").addEventListener("click", function(){
     document.getElementById("testing").style.display = "none"
     modal.style.display = "none"
