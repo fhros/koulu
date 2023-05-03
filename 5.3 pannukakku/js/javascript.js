8bb2f947ad75ea87bce68ea9b02ffa34448eee92
fhros <112174015+fhros@users.noreply.github.com>
2023-05-03 09:27:45 +0300

    pannukakku update

--- 5.3 pannukakku/js/javascript.js
+++ 5.3 pannukakku/js/javascript.js
@@ -1,12 +1,14 @@
+//global jutskat
 const question1 = prompt("lempi ruokasi")
-const question2 = prompt("🥞 pannukakku")
+const question2 = alert("🥞 pannukakku")
 const question3 = prompt("kai laitoit edelliseen pannukakku")
-
 var text = document.getElementById("text")
 const doggie = document.body.style.backgroundImage = "url('dog.gif')"
+var a = true
 
+//tää juttu en muista mikä tää oli
 const person = {
-    "cake": question2,
+    "cake": question1,
     speak() {
         text.innerText = `${this.cake} on paras`
     },
@@ -14,13 +16,13 @@ const person = {
 }
 person.speak()
 
-if(question2 == "pannukakku") {
+//jos laitoit pannukakku tokaan kysymykseen
+if(question1 == "pannukakku") {
     alert("🎉🎉🎉")
 }
 
-var a = true
-
-if(question2 != "pannukakku") {
+//jos et laittanut pannukakku tokaan kysymykseen
+if(question1 != "pannukakku") {
     alert("ai et laittanut")
     alert("no voi voi")
     while(a == true) {
