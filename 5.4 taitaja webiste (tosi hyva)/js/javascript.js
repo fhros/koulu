3d521c89144416cdb1d71688383e636797052811
fhros <112174015+fhros@users.noreply.github.com>
2023-05-04 13:03:36 +0300

    Add files via upload

---
+++ 5.4 taitaja webiste (tosi hyva)/js/javascript.js
@@ -0,0 +1,19 @@
+//ku klikkaa koiraa jotai tapahtuu :)
+
+var jekku = document.getElementById("kuva1").addEventListener("click", function(){
+    window.location.href = "jekku.html"
+})
+
+//napit ei tee mitää pitää alerttaa
+
+document.getElementById("pageBtn").addEventListener("click", function(){
+    alert("en tee viel mitään :(")
+})
+
+document.getElementById("navBtn").addEventListener("click", function(){
+    alert("en tee viel mitään :(")
+})
+
+document.getElementById("hamburger").addEventListener("click", function(){
+    alert("en tee viel mitään :(")
+})
