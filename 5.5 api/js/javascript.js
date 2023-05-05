ddd75b194e76031ab8e2977026a01eab39c29e17
fhros <112174015+fhros@users.noreply.github.com>
2023-05-05 11:02:23 +0300

    Update javascript.js

--- 5.5 api/js/javascript.js
+++ 5.5 api/js/javascript.js
@@ -1,5 +1,8 @@
+//ottaa apin
 fetch("https://v2.jokeapi.dev/joke/Any?safe-mode&lang=en&type=twopart")
+//tekee vastauksesta .json
 .then(response => response.json())
+//lisää sen sivulle ja näyttää konsolissa
 .then(joke => {
     console.log(joke)
     document.getElementById("setup").innerText = joke.setup
