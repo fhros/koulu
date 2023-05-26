2d64b78a8188e57cf76a4a90204628f8ddf5b4d7
fhros <112174015+fhros@users.noreply.github.com>
2023-05-26 13:40:33 +0300

    Add files via upload

--- 5.25 kirjain animatio/js/javascript.js
+++ 5.25 kirjain animatio/js/javascript.js
@@ -1,13 +1,25 @@
-var lettersArray = document.querySelector("h1").innerText.split("")
+const h1 = document.querySelector("h1")
+    const text = h1.innerText.split('')
+    let html = ""
 
-var container = document.getElementById("container")
+    text.forEach(letter => {
+        let classes = ''
+        if(letter !== ' ') {
+       classes = 'letter js-letter' 
+    }
+        html = html + `<span class='${classes}'>${letter}</span>`
+    })
 
-for (let i = 0; i < lettersArray.length; i++) {
-    const letterElement = document.createElement("span")
-
-    letterElement.classList.add('letter-' + (i + 1))
-
-    letterElement.textContent = lettersArray
-
-    letterElement.appendChild
-}
+    h1.innerHTML = html
+    const jsLetters = document.querySelectorAll(".js-letter")
+    jsLetters.forEach(node => {
+        node.addEventListener("mouseover", function(event) {
+            console.log(this.innerText)
+            this.classList.add("active")
+            document.body.style.backgroundImage = "url('dog.gif')"
+        })
+        node.addEventListener("mouseout", function(event) {
+            this.classList.remove("active")
+            document.body.style.backgroundImage = ""
+        })
+    })
